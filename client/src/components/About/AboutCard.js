611f58d36a80c94eec64598a5a2f151a448acc44
Jasubrek Yuldashov <jasurbekyuldashov123@gmail.com>
2025-07-27 01:18:26 +0500

    push

--- client/src/components/About/AboutCard.js
+++ client/src/components/About/AboutCard.js
@@ -17,9 +17,9 @@ export default function AboutCard() {
                         </div>
                         <div className="md:w-1/2  md:pl-8 flex flex-col md:items-start md:text-left items-center">
                             {/* <h1 className="title-font lg:text-3xl text-2xl mb-4 font-bold text-gray-900">
-                                “Aspires to be a pre-eminent deemed university of national standing in education and innovation”.
+                                "Aspires to be a pre-eminent deemed university of national standing in education and innovation".
                             </h1> */}
-                            <p className=" leading-relaxed font-medium text-sm sm:text-lg p-2">Асака туман 2-сон политехникуми (собиқ Асака қишлоқ хўжалик касб-ҳунар коллежи) 1999 йил сентябр ойида ташкил топган. Ўзбекистон Республикаси Вазирлар Маҳкамасининг 1998 йил 23 сентябрдаги 406-сонли қарорига мувофиқ таълим муассасаси ташкил топган. <span className='font-bold'>Таълим муассаса 1999 йил 28 июлда 259/1 сон билан давлат рўйхатидан ўтказилган.</span></p>
+                            <p className=" leading-relaxed font-medium text-sm sm:text-lg p-2">Asaka tuman 2-son politexnikumi (sobiq Asaka qishloq xo'jalik kasb-hunar kolleji) 1999 yil sentyabr oyida tashkil topgan. O'zbekiston Respublikasi Vazirlar Mahkamasining 1998 yil 23 sentyabrdagi 406-sonli qaroriga muvofiq ta'lim muassasasi tashkil topgan. <span className='font-bold'>Ta'lim muassasa 1999 yil 28 iyulda 259/1 son bilan davlat ro'yxatidan o'tkazilgan.</span></p>
                         </div>
                     </div>
                 </section>
@@ -30,12 +30,12 @@ export default function AboutCard() {
                 <section className="text-gray-600 body-font ">
                     <div className="mx-auto flex md:px-5 md:flex-row-reverse justify-center flex-col items-center">
                         {/* <div className="md:max-w-sm lg:max-w-md md:h-full w-full h-48 overflow-hidden flex justify-center items-center mb-4 md:mb-0"> */}
-                            {/* <img className="w-full md:rounded-lg" alt="hero" src="/images/collegepic(1).jpg" /> */}
+                        {/* <img className="w-full md:rounded-lg" alt="hero" src="/images/collegepic(1).jpg" /> */}
                         {/* </div> */}
                         <div className="md:w-1/2 md:pr-8 flex flex-col md:items-start md:text-left items-center">
-                            {/* <h1 className="title-font lg:text-3xl text-2xl mb-4 font-bold text-gray-900">“Aspires to be a pre-eminent deemed university of national standing in education and innovation”.
+                            {/* <h1 className="title-font lg:text-3xl text-2xl mb-4 font-bold text-gray-900">"Aspires to be a pre-eminent deemed university of national standing in education and innovation".
                             </h1> */}
-                            <p className="mb-8 leading-relaxed font-medium text-sm sm:text-lg p-2">Ўзбекистон Республикаси Президентининг 2019 йил 6 сентябрдаги ПФ-5812-сонли Фармони, Андижон вилоят касбий таълимни ривожлантириш ва мувофиқлаштириш бошқармасининг 2020 йил 1 июндаги 251-сонли буйруғи ҳамда қишлоқ хўжалик касб-ҳунар коллежининг 2020 йил 23 июндаги 01-01-365-сонли хатига асосан Асака туман ҳокимининг 27.06.2020 йилдаги 496-қ сонли қарори билан Асака қишлоқ хўжалик касб-ҳунар коллежи Асака туман 2-сон касб-ҳунар мактабига ўзгартирилган. Ўзбекистон Республикаси Президентининг 2024 йил 16 октябрдаги 158-сонли Фармони билан Асака туман 2-сон касб-ҳунар мактаби Асака туман 2-сон Политехникумига ўзгартирилган ва ҳозирда шу ном билан фаолият олиб бормоқда.</p>
+                            <p className="mb-8 leading-relaxed font-medium text-sm sm:text-lg p-2">O'zbekiston Respublikasi Prezidentining 2019 yil 6 sentyabrdagi PF-5812-sonli Farmoni, Andijon viloyat kasbiy ta'limni rivojlantirish va muvofiqlashtirish boshqarmasining 2020 yil 1 iyundagi 251-sonli buyrug'i hamda qishloq xo'jalik kasb-hunar kollejining 2020 yil 23 iyundagi 01-01-365-sonli xatiga asosan Asaka tuman hokimining 27.06.2020 yildagi 496-q sonli qarori bilan Asaka qishloq xo'jalik kasb-hunar kolleji Asaka tuman 2-son kasb-hunar maktabiga o'zgartirilgan. O'zbekiston Respublikasi Prezidentining 2024 yil 16 oktyabrdagi 158-sonli Farmoni bilan Asaka tuman 2-son kasb-hunar maktabi Asaka tuman 2-son Politexnikumiga o'zgartirilgan va hozirda shu nom bilan faoliyat olib bormoqda.</p>
                         </div>
                     </div>
                 </section>
